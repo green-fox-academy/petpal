d96cb564e581c1d06d974c9c711ff24d5d672281
bleaksmile <toeraab.avf@gmail.com>
2019-03-05 20:04:38 +0100

    oauth2 implemented

--- src/main/java/com/greenfoxacademy/petpal/PetpalApplication.java
+++ src/main/java/com/greenfoxacademy/petpal/PetpalApplication.java
@@ -2,10 +2,35 @@ package com.greenfoxacademy.petpal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class PetpalApplication {
+@RestController
+@EnableOAuth2Sso
+public class PetpalApplication extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+            .csrf().disable()
+            .antMatcher("/**")
+            .authorizeRequests()
+            .antMatchers("/", "/login**", "/webjars/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and().logout().logoutSuccessUrl("/").permitAll();
+  }
 
+  @RequestMapping("/user")
+  public Principal user(Principal principal) {
+    return principal;
+  }
   public static void main(String[] args) {
     SpringApplication.run(PetpalApplication.class, args);
   }
