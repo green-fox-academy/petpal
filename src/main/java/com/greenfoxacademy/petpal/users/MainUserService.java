a4d71ca75ccb75902b63c502ec062ed4b8223408
Peter Kovacs <pitner33@gmail.com>
2019-03-06 22:47:26 +0100

    (fix) repository change

--- src/main/java/com/greenfoxacademy/petpal/users/MainUserService.java
+++ src/main/java/com/greenfoxacademy/petpal/users/MainUserService.java
@@ -10,13 +10,13 @@ import java.util.Set;
 public interface MainUserService <T extends SuperUser>{
 
   Optional<T> findByUsername(String username);
-  T findById(Long id) throws UserIdNotFoundException;
+  T findById(Long id) throws Throwable;
 
   T saveUser(T t) throws UserIsNullException;
 
   void removeUser(Long id) throws UserIdNotFoundException;
   void checkIfUserIsnull(T t) throws UserIsNullException;
 
-  Set<Animal> ownedAnimalsByUser(Long userId) throws UserIdNotFoundException;
+  Set<Animal> ownedAnimalsByUser(Long userId) throws Throwable;
 
 }
