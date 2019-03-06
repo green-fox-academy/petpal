392a5ec931171ae32926bdf8310da469ed08d386
bleaksmile <toeraab.avf@gmail.com>
2019-03-06 23:14:22 +0100

    cors added

--- src/main/java/com/greenfoxacademy/petpal/users/UserController.java
+++ src/main/java/com/greenfoxacademy/petpal/users/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "https://petpalgf.herokuapp.com/", maxAge = 3600)
 public class UserController {
 
   private PrivateUserService privateUserService;
