09be957c536b4ca8ab26ffd7b1fb13eaface455f
ChrisFrill <fkrisztian93@gmail.com>
2019-03-06 09:38:10 +0100

    Changed generation type

--- src/main/java/com/greenfoxacademy/petpal/users/SuperUser.java
+++ src/main/java/com/greenfoxacademy/petpal/users/SuperUser.java
@@ -13,7 +13,7 @@ import java.util.List;
 public abstract class SuperUser {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String username;
   private String password;
