7108b4694133d0a5c4db44f440023ef4a14eb3fa
ChrisFrill <fkrisztian93@gmail.com>
2019-03-06 10:48:37 +0100

    Added animal exceptions

---
+++ src/main/java/com/greenfoxacademy/petpal/exception/AnimalIsNullException.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.petpal.exception;
+
+public class AnimalIsNullException extends Exception {
+  public AnimalIsNullException(String message) {
+    super(message);
+  }
+}
