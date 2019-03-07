72ab1c5f7cfab781a3410bc1157751e05617e4bf
ChrisFrill <fkrisztian93@gmail.com>
2019-03-07 08:19:53 +0100

    Added exception handling

--- src/main/java/com/greenfoxacademy/petpal/exception/UserNotFoundException.java
+++ src/main/java/com/greenfoxacademy/petpal/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.greenfoxacademy.petpal.exception;
 
-public class UserIdNotFoundException extends Exception {
-  public UserIdNotFoundException(String message) {
+public class UserNotFoundException extends Exception {
+  public UserNotFoundException(String message) {
     super(message);
   }
 }
