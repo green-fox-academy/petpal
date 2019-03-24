372246a04868272f6c9a9693d25fe10cb7d0d2b9
Dani Zsin <dani.zsinx@gmail.com>
2019-03-24 22:29:33 +0100

    fav animals add

--- src/main/js/src/components/favourites/LikedAnimals.js
+++ src/main/js/src/components/favourites/LikedAnimals.js
@@ -10,13 +10,29 @@ const LikedAnimals = ({ listLikedAnimalsRequest, likedAnimals }) => {
 
   return likedAnimals ? (
     <ul className="favanims">
+      <li>
+        <ul>
+          <li>name</li>
+          <li>type</li>
+          <li>gender</li>
+          <li>photo</li>
+          <li>remove</li>
+        </ul>
+      </li>
       {likedAnimals.map(animal => (
         <li key={animal.id}>
           <ul>
             <li>{animal.name}</li>
             <li>{animal.type}</li>
             <li>{animal.gender}</li>
-            <li>{animal.photoPath}</li>
+            <li>
+              <img src={`/assets/${animal.photoPath}`} alt="animal" />
+            </li>
+            <li>
+              <button data-animalid={animal.id} className="removebtn" type="button">
+                <i className="fas fa-trash-alt" data-animalid={animal.id} />
+              </button>
+            </li>
           </ul>
         </li>
       ))}
--- src/main/js/src/components/favourites/OwnedAnimals.js
+++ src/main/js/src/components/favourites/OwnedAnimals.js
@@ -8,15 +8,44 @@ const OwnedAnimals = ({ listOwnedAnimalsRequest, ownedAnimals }) => {
     listOwnedAnimalsRequest();
   }, []);
 
+  const handleOwnedAnimalClick = event => {
+    const { dataset } = event.target;
+    if (dataset.animalid) {
+      dataset.animalaction === 'edit' ? console.log('editme', dataset.animalid) : console.log('removeme', dataset.animalid);
+    }
+  };
+
   return ownedAnimals ? (
-    <ul className="favanims">
+    <ul className="favanims" onClick={handleOwnedAnimalClick}>
+      <li>
+        <ul>
+          <li>name</li>
+          <li>type</li>
+          <li>gender</li>
+          <li>photo</li>
+          <li>edit</li>
+          <li>remove</li>
+        </ul>
+      </li>
       {ownedAnimals.map(animal => (
         <li key={animal.id}>
           <ul>
             <li>{animal.name}</li>
             <li>{animal.type}</li>
             <li>{animal.gender}</li>
-            <li>{animal.photoPath}</li>
+            <li>
+              <img src={`/assets/${animal.photoPath}`} alt="animal" />
+            </li>
+            <li>
+              <button data-animalid={animal.id} data-animalaction="edit" type="button">
+                <i data-animalid={animal.id} data-animalaction="edit" className="fas fa-edit" />
+              </button>
+            </li>
+            <li>
+              <button data-animalid={animal.id} data-animalaction="remove" className="removebtn" type="button">
+                <i data-animalid={animal.id} data-animalaction="remove" className="fas fa-trash-alt" />
+              </button>
+            </li>
           </ul>
         </li>
       ))}
