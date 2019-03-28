dd3d58417e3cca0dd5f174b5781537692961895a
ChrisFrill <fkrisztian93@gmail.com>
2019-03-28 16:30:25 +0100

    Added ChatIdNotFoundException

---
+++ src/main/java/com/greenfoxacademy/petpal/exception/ChatIdNotFoundException.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.petpal.exception;
+
+public class ChatIdNotFoundException extends Exception {
+  public ChatIdNotFoundException(String message) {
+    super(message);
+  }
+}
