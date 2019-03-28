d67e3d822dbb75add81e3bbbfe593f73e5a7bd48
ChrisFrill <fkrisztian93@gmail.com>
2019-03-28 16:31:36 +0100

    Added ChatMessage class

---
+++ src/main/java/com/greenfoxacademy/petpal/chat/models/ChatMessage.java
@@ -0,0 +1,34 @@
+package com.greenfoxacademy.petpal.chat.models;
+
+import com.greenfoxacademy.petpal.users.models.ParentUser;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+  @JoinColumn(name = "author_id", referencedColumnName = "id")
+  private ParentUser author;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "chat_id", referencedColumnName = "id")
+  private Chat chat;
+
+  private String message;
+
+  private Timestamp sentAt;
+
+}
