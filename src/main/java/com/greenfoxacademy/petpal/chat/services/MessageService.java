5d51c5d6834677bf4f1e9ee063b83e8f90be64e8
ChrisFrill <fkrisztian93@gmail.com>
2019-03-28 16:32:32 +0100

    Added MessageService

---
+++ src/main/java/com/greenfoxacademy/petpal/chat/services/MessageService.java
@@ -0,0 +1,20 @@
+package com.greenfoxacademy.petpal.chat.services;
+
+import com.greenfoxacademy.petpal.chat.models.ChatMessage;
+import com.greenfoxacademy.petpal.chat.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+  private MessageRepository messageRepository;
+
+  @Autowired
+  public MessageService(MessageRepository messageRepository) {
+    this.messageRepository = messageRepository;
+  }
+
+  public void saveMessage(ChatMessage chatMessage) {
+    messageRepository.save(chatMessage);
+  }
+}
