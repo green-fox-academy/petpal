b3b10ed6f7b62fc2b4d0c0ea0bcb32e591f04ef6
Peter Kovacs <pitner33@gmail.com>
2019-03-29 09:58:30 +0100

    (fix) AnimalController tests

--- src/main/java/com/greenfoxacademy/petpal/animal/models/AnimalDTO.java
+++ src/main/java/com/greenfoxacademy/petpal/animal/models/AnimalDTO.java
@@ -1,15 +1,13 @@
 package com.greenfoxacademy.petpal.animal.models;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.sql.Timestamp;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AnimalDTO {
 
--- src/test/java/com/greenfoxacademy/petpal/ParentUser/Constants.java
+++ src/test/java/com/greenfoxacademy/petpal/ParentUser/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
   static final String registerEndpoint = "/register/user";
   static final String loginEndpoint = "/login/user";
+
+
 }
